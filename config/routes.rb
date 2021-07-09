d877309f4fe84d4d3d0134d2916375c0e4d751a3
savasan2 <83535755+savasan2@users.noreply.github.com>
2021-07-09 19:24:30 +0000

    ユーザー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  resources :users
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20210709150412_add_column_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :image_id, :string
+    add_column :users, :Genre_id, :integer
+    add_column :users, :introduction, :text
+  end
+end
