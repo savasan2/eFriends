ef26b7432f74d40b9085e1c396db8e8d16c60b13
savasan2 <83535755+savasan2@users.noreply.github.com>
2021-07-11 09:42:50 +0000

    match

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_09_194308) do
+ActiveRecord::Schema.define(version: 2021_07_11_074452) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_07_09_194308) do
   end
 
   create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
